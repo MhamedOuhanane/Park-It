d8423ebfcb8ce7155bd4fffc6e1a993a81af5d57
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-03-10 15:28:44 +0000

    Créer les fichier de modeles, contoleurs et les migration neccessaires pour le projet

---
+++ app/Http/Controllers/AdministrateurController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Administrateur;
+use Illuminate\Http\Request;
+
+class AdministrateurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Administrateur $administrateur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Administrateur $administrateur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Administrateur $administrateur)
+    {
+        //
+    }
+}
